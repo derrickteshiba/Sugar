52e4ae18ff72afcaa0c8f67c300bbdacf8b1a2bd
jasonrinaba <jasonrinaba@g.ucla.edu>
2022-02-21 15:47:16 -0800

    Filter for MatchID

--- src/components/app/matchProfile.jsx
+++ src/components/app/matchProfile.jsx
@@ -1,11 +1,73 @@
 import { getDatabase, ref as dbRef, get } from "firebase/database"
 import React, { useState, useEffect } from 'react'
-import { Accordion, Row, Col, Image, Container, Card} from 'react-bootstrap'
+import { Accordion, Row, Col, Image, Container, Card, Button} from 'react-bootstrap'
 import { getProfilePicUrl } from "../../utils/storage"
 
 const db = getDatabase()
 
-export default function MatchProfile( {uid} ) {
+export function MatchProfilePending( {uid} ) {
+    const picRef = getProfilePicUrl(uid)
+
+    const [profile, setProfile] = useState({
+        data: null,
+        loading: true
+    })
+    useEffect(() => {
+        const userRef = dbRef(db, "user/" + uid)
+    
+        return get(userRef).then((snap) => {
+          setProfile({
+            data: {
+              ...snap.val()
+            },
+            loading: false
+          })
+        })
+    }, [uid])
+
+    if (!profile.data)
+    {
+        console.log("no data")
+        return (
+            <p>ERROR: no data</p>
+        )
+    }
+
+    return (
+    <>
+        <Button id = {uid} variant="success">Accept</Button>{' '}
+        <Button variant="danger">Reject</Button>{' '}
+        <Accordion defaultActiveKey="0" style={{ width: '70%' }}>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                <Row style={{ width: '100%' }}>
+                    <Col  xs={4}> 
+                        <Image style={{ width: '200px', height: '200px', borderRadius: '50%', overflow: 'hidden'}} src={picRef} alt="Match's Profile Picture" />
+                    </Col>
+                    <Col className='d-flex justify-content-center align-self-center'>
+                        <Row>
+                            <h1>{profile.data.name}</h1>
+                        </Row>
+                        {/* <Row> 
+                            <Button variant="success">Accept</Button>{' '}
+                            <Button variant="danger">Reject</Button>{' '}
+                        </Row> */}
+                    </Col> 
+                </Row>
+                </Accordion.Header>
+                <Accordion.Body>
+                    <h5>Bio:</h5>
+                    <p>{profile.data.bio}</p>
+                    <h5>Status:</h5>
+                    <p>{profile.data.status}</p>
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+     </>
+  )
+}
+
+export function MatchProfileAccepted( {uid} ) {
     const picRef = getProfilePicUrl(uid)
 
     const [profile, setProfile] = useState({
--- src/components/app/matches.jsx
+++ src/components/app/matches.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Container, Row} from 'react-bootstrap'
-import MatchProfile from "./matchProfile"
+import {MatchProfilePending, MatchProfileAccepted} from "./matchProfile"
 
 var user = "Jason"
 
 var matches = [
-    { to:"Jason", from:"Krish", status: "pending" },
-    { to:"Krish", from:"Derrick", status: "pending" },
-    { to:"Derrick", from:"Matthew", status: "pending" },
-    { to:"Matthew", from:"Victor", status: "accepted" },
-    { to:"Victor", from:"Jason", status: "accepted" },
-    { to:"Jason", from:"Matthew", status: "accepted" }
+    { to:"Jason", from:"Krish", status: "pending", matchID: "1"},
+    { to:"Krish", from:"Derrick", status: "pending", matchID: "2"},
+    { to:"Derrick", from:"Matthew", status: "pending", matchID: "3"},
+    { to:"Matthew", from:"Victor", status: "accepted", matchID: "4"},
+    { to:"Victor", from:"Jason", status: "accepted", matchID: "5"},
+    { to:"Jason", from:"Matthew", status: "accepted", matchID: "6"}
 ];
 
 const pendingMatches = matches.filter(match =>(
@@ -26,10 +26,10 @@ function statusFilter(name, Array){
     var res = [];
     for (var i=0; i < Array.length; i++) {
         if (Array[i].to === name) {
-            res.push(Array[i].from);
+            res.push([Array[i].from, Array[i].matchID]);
         }
         else if (Array[i].from === name) {
-            res.push(Array[i].to)
+            res.push([Array[i].to, Array[i].matchID]);
         }
     }
     return res;
@@ -54,42 +54,17 @@ export default function Matches({uid}) {
         <Container>
             <h3>Pending:</h3>
             <Row className="justify-content-md-center">
-                {pendingNames.map(d => (<p>{d}</p>))} 
+                <MatchProfilePending uid="bPcbMZMxzVZUIwfgGH6wKCY7qmG3" />
+                {pendingNames.map(d => (<p>{d[0]}</p>))} 
             </Row> 
         </Container>
         <Container>
             <h3>MATCHED!!!</h3>
             <Row className="justify-content-md-center">
-                {acceptedNames.map(d => (<p>{d}</p>))} 
+                <MatchProfileAccepted uid={uid}/>
+                {acceptedNames.map(d => (<p>{d[0]}</p>))} 
             </Row> 
         </Container>
      </>
   )
 }
-
-// export default function Matches({uid}) { 
-
-//     return (
-//     <>
-//         <br />  
-//         <div className="text-center">
-//             <h1>
-//                 Your Matches 
-//             </h1> 
-//         </div>
-//         <br />
-//         <Container>
-//             <h3>Pending:</h3>
-//             <Row className="justify-content-md-center">
-//                 <MatchProfile uid={uid}/>
-//             </Row> 
-//         </Container>
-//         <Container>
-//             <h3>MATCHED!!!</h3>
-//             <Row className="justify-content-md-center">
-//                 <MatchProfile uid={uid}/>
-//             </Row> 
-//         </Container>
-//      </>
-//   )
-// }
